84785c17a8bc516cddee81408e9e9ba07781108a
kkh9372 <kkh9372@naver.com>
2021-08-09 16:56:28 +0900

    08/09 파이썬 실습

---
+++ Ch02/2_2_DataType.py
@@ -0,0 +1,52 @@
+
+"""
+날짜 : 2021/08/09
+이름 : 김관후
+내용 : 파이썬 자료형 실습하기 교재 p37
+"""
+
+#숫자형(int)
+var1 = 1
+var2 = 2
+var3 = -3
+
+print('var1 :', var1)
+print('var2 :', var2)
+print('var3 :', var3)
+print('var1 type :', type(var1))
+print('var1 type :', type(var2))
+print('var1 type :', type(var3))
+
+#실수형(float)
+var4 = 0.4
+var5 = -1.1234
+print('var4 :', var4)
+print('var5 :', var5)
+print('var4 type :', type(var4))
+print('var5 type :', type(var5))
+
+#논리형(bool)
+var6 = True
+var7 = False
+
+print('var6 :', var6)
+print('var7 :', var7)
+print('var6 type :', type(var6))
+print('var7 type :', type(var7))
+
+#문자열(string)
+var8 = 'A'
+var9 = "A"
+var10 = 'Apple'
+var11 = "Apple"
+print('var 8 :', var8)
+print('var 9 :', var9)
+print('var 10 :', var10)
+print('var 11 :', var11)
+print('var8 type :', type(var8))
+print('var9 type :', type(var9))
+print('var10 type :', type(var10))
+print('var11 type :', type(var11))
+
+#숫자형(int)
+#숫자형(int)
